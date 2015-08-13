b4cd9c30d9a89ad18be0a4ed5c5eeaf27c5f9f31
Gus <yo@soygus.com>
2015-08-13 14:01:42 -0500

    GLobal funcs

--- .guides/bashtests/cli-2-1.sh
+++ .guides/bashtests/cli-2-1.sh
@@ -8,95 +8,13 @@ hist_file="$BASHDIR/bashtests/$check_file.txt"
 
 echo "$check_file" >> $bash_history
 grep -A2000 -e "^$check_file" $bash_history > "$BASHDIR/bashtests/${check_file}.txt"
+find "$hist_file" -type f -exec sed -i "s@~@$HOME@g" {} \; 
 
 # Must match for erasing history
 RES_HIST=0
 COUNT=0
 QCOUNT=1
 
-# Reset history
-function reset_history {
-	if [[ $RES_HIST -eq $QCOUNT ]]; then
-		echo "$arg" > ~/.bash_history
-	fi
-}
-
-function expect_command 
-{
-	if grep -Fxq "$1" "$hist_file"
-	then
-		response "$2" $COUNT
-	else 
-		tell_error "$2" $COUNT
-	fi
-}
-
-function expect_commands 
-{
-	args_array=()
-	for (( i = 2; i <= $#; i++ )); do
-		args_array[i]=${!i}
-	done
-	for (( i = 2; i <= $#; i++ )); do		
-		if grep -Fxqe "${args_array[$i]}" "$hist_file" || grep -Fxqe "${args_array[$i]}/" "$hist_file" || grep -Fxqe "${args_array[$i]} " "$hist_file" || grep -FxE "${args_array[$i]}" "$hist_file"
-		then
-			found_arg="${args_array[$i]}"
-			response "$1" $COUNT
-			return
-		else
-			tell_error "$1" $COUNT
-			return
-		fi
-	done
-}
-
-function expect_file 
-{
-	if [[ -f "$1" ]]; then
-		response "$2" $COUNT
-	else 
-		tell_error "$2" $COUNT
-	fi
-}
-
-function expect_directory
-{
-	if [[ -d "$1" ]]; then
-		response "$2" $COUNT
-	else 
-		tell_error "$2" $COUNT
-	fi
-}
-
-function expect_directories
-{
-	args_array=()
-	for (( i = 1; i <= $#; i++ )); do
-		args_array[i]=${!i}
-		found_arg="${args_array[$i]}"
-		if [[ $i -le $# && -d "${args_array[$i]}" ]]; then
-			echo -e "[Correct] Task $COUNT. ${found_arg}"
-		else
-			echo -e "[Missing] Task $COUNT. Expected: ${found_arg}"
-            return 1
-		fi
-	done
-	test_command
-}
-
-function tell_error 
-{
-	echo -e "[Error  ] Task 1. Expected: $1. Try again."
-	test_command
-	return 1
-}
-
-function response 
-{
-	echo -e "[Correct] Task 1. ${1}"
-	(( RES_HIST ++ ))
-	test_command
-}
 
 function test_command {
 	(( COUNT ++ ))
